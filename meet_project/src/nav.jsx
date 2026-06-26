1cb16d0fd87d9b2cd70984d33527188339b347ff
Kasemsak Ratchanipan <gmjohn200gg@gmail.com>
2026-06-27 04:08:37 +0700

    nav  up dete

--- meet_project/src/nav.jsx
+++ meet_project/src/nav.jsx
@@ -1,8 +1,13 @@
 import'./slty_nav.css'
+import img from'./assets/img/Suer-re.png'
 const Nav = () => {
   return (
     <nav>
       <ul>
+      <li className="logo">
+        <img src={img} alt="Logo" />
+      </li>
+        <li className='logo'><h1 href="">Top adge</h1></li>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
